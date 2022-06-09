b9cc0fab43d73e337d832231a044b909042c4aae
Usuario <Usuario@Usuario-PC>
2022-06-09 14:57:57 -0500

    correccion

--- cv_ejercicio/src/cv_ejercicio/Cv_ejercicio.java
+++ cv_ejercicio/src/cv_ejercicio/Cv_ejercicio.java
@@ -8,9 +8,6 @@ public class Cv_ejercicio {
     public static void main(String[] args) {
         System.out.println("versión 0");
         System.out.println("versión 1");
-        System.out.println("versión 2");
-        System.out.println("versión 3");
-        System.out.println("versión 4");
     }
     
 }
